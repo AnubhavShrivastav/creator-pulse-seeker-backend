1c448d6062bff5e44682ed20fb0d7b53b00843ea
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2025-10-03 12:27:02 +0530

    refactor: improve user retrieval logic and enhance logging

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,64 +2,66 @@ import { ApiResponse } from "../utils/apiResponse";
 import { ApiError } from "../utils/apiError";
 import axios from "axios";
 import Creator from "../models/creator.schema";
-import { data } from "../constant";
+// import { data } from "../constant";
 import type { Request, Response } from "express";
 
+const findUserByUsername = async (username: any) => {
+  const response: any = await axios.get(
+    `${process.env.SERVER_URL}${username}&${process.env.ACCESS_KEY}`
+  );
+
+  return response.data;
+};
+
 const getUser = async (req: Request, res: Response) => {
+  // 1. get username from frontend
   const { username } = req.query;
-  console.log(username);
+  console.log("Username coming from frontend:", username);
 
   try {
-    let findUser = await Creator.findOne({ username: username });
-    console.log("user found: ", findUser);
+    //2. find the user from db
+    let User = await Creator.findOne({ username: username });
+    console.log("user found: ", User);
 
-    if (findUser) {
-      const userSavedinDBDate = findUser?.createdAt?.toDateString();
+    if (User) {
+      // 4. if user is available in db, then get the user createdAt Date.
+      const UserCreatedAt = User?.createdAt?.toDateString();
       const userCurrentDate = new Date().toDateString();
-      console.log(userSavedinDBDate);
-      console.log(userCurrentDate);
+      console.log("User createdAT from DB: ", UserCreatedAt);
+      console.log("User current date", userCurrentDate);
 
-      if (userSavedinDBDate != userCurrentDate) {
-        const response: any = await axios.get(
-          `${process.env.SERVER_URL}${username}&${process.env.ACCESS_KEY}`
+      // 5. compare the user createdAt date and current date.
+      if (UserCreatedAt != userCurrentDate) {
+        // 6. if user createdAt date is more than one day, then find the user latest data using fetch and replace new response
+        //   with previous response and send the response
+
+        let userNewData = await findUserByUsername(username);
+        console.log("get latest data of User from fetch", userNewData);
+
+        const userKey = User.pk;
+        console.log("get User key from db", userKey);
+
+        let replaceData = await Creator.replaceOne(
+          { pk: userKey },
+          userNewData
         );
-        const userKey = findUser.pk;
-        const newData = response.data;
-        console.log(userKey);
-        console.log(newData);
+        console.log("replace the data in DB", replaceData);
 
-        const replacedata = await Creator.replaceOne({ pk: userKey }, newData);
-        return res
-          .status(200)
-          .json(
-            new ApiResponse(
-              200,
-              { newData, replacedata },
-              "updated the data in db with latest data"
-            )
-          );
-      } else {
-        return res
-          .status(200)
-          .json(
-            new ApiResponse(200, { findUser }, "User Found Successfully in DB")
-          );
+        User = userNewData;
+        console.log("new Data of User:", User);
       }
     } else {
-      const response: any = await axios.get(
-        `${process.env.SERVER_URL}${username}&${process.env.ACCESS_KEY}`
-      );
-      console.log("fromfetch: ", response.data);
-
-      const createUser = await Creator.create(response.data);
-      console.log("create User: ", createUser);
-
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(200, { createUser }, "new creator added in the db")
-        );
+      // 3. if user in not available in db, then create the new User in db
+      const newUser = await findUserByUsername(username);
+      let createUser = await Creator.create(newUser);
+      User = createUser;
+      console.log("User is not found in db and create new User: ", User);
     }
+
+    // 7. if user createdAt date is less than one day, then send response
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { User }, "creator added in the db"));
   } catch (error: any) {
     console.log("error fetching data", error);
     res.status(400).json(new ApiError(400, "failed to fetched the data"));
@@ -67,3 +69,37 @@ const getUser = async (req: Request, res: Response) => {
 };
 
 export { getUser };
+
+/**
+ * 
+ * --> 1. get username from frontend
+ * --> 2. find the user from db
+ * --> 3. if user in not available in db, then create the new User in db
+ * --> 4. if user is available in db, then get the user createdAt Date.
+ * --> 5. compare the user createdAt date and current date.
+ * --> 6. if user createdAt date is more than one day, then find the user data using fetch and replace response
+          with previous response and send the new response
+ * --> 7. if user createdAt date is less than one day, then send response 
+   
+
+   let user = foundUser??{}
+
+  //date match 
+  res -> user
+
+  //date unmatch
+  apiuser-> axios
+  user=apiuser
+
+  // user 
+  apiuser-> axios
+  user=apiuser
+
+
+  res.send(user)
+
+
+// findUserbyUsernma(username)
+
+
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,38 @@
-import express from "express"
-import connectDB from "../src/config/db"
-import dotenv from "dotenv"
-import user from "./routes/user.routes"
-import cors from "cors"
+import express from "express";
+import connectDB from "../src/config/db";
+import dotenv from "dotenv";
+import user from "./routes/user.routes";
+import cors from "cors";
 import type { Request, Response } from "express";
 
- 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
-  origin: "https://instanalyticscom.netlify.app"
-}))
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+  })
+);
 
 dotenv.config({
   path: ".env",
 });
 
-app.use("/api", user)
+app.use("/api", user);
 
-app.get("/" ,(_req: Request,res: Response) => {
- res.send("server is running")
-})
+app.get("/", (_req: Request, res: Response) => {
+  res.send("server is running");
+});
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT ,() => {
-    console.log(`server is running on the port http://localhost:${process.env.PORT}`)
-})
-})
-.catch((err) => {
-    console.log("MongoDb Connection Error", err)
-})
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `server is running on the port http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb Connection Error", err);
+  });
